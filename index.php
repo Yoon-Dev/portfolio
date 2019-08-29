87e25fa665cfaba19cfefee64e36fa6e184b94bd
mo_high <pierre.monier99@gmail.com>
2019-08-29 12:13:56 +0200

    initialisation du projet

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./bootstrap-4.3.0-dist/css/bootstrap.css"></script>
+    <script src="./bootstrap-4.3.0-dist/css/bootstrap-grid.css"></script>
+    <script src="./css/style.css"></script>
+    <title>Pierre Monier</title>
+</head>
+<body>
+<div class="container-fluid">
+<div class="row">
+<div class="col-3 petite"><h1>petit a disparaitre</h1></div>
+<div class="col-9 grande"><h1>principame</h1></div>
+</div>
+</div>
+</body>
+</html>
