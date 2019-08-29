df7e0930cd16d7bd455434b2a3d9536fd11d42a9
mo_high <pierre.monier99@gmail.com>
2019-08-29 18:10:41 +0200

    base responsive, lien nav non fait, pas jolie

---
+++ pierre_monier.php
@@ -0,0 +1,19 @@
+<?php
+
+function activités() {
+	$étudiant = [ 'IUT', 'MMI', '2ème années' ];
+}
+
+function workflow($favoris) {
+	$ide = 'VisualStudio Code';
+	$env = 'Wamp';
+	$tools = ['Bootstrap', 'Jquery'];
+	$autres = 'Git';
+}
+
+$hobbies = [
+	'sports' => ['Course à pied', 'Basket'],
+	'art' => ['Cinéma', 'Gastronomie', 'Peinture']
+];
+
+?>
